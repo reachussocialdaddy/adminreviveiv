0a61f28fdaf6a165f6bdce83012b33298a11d6d7
Aman Mishra <thegreatestaman999@gmail.com>
2026-04-26 03:00:57 +0530

    \Update API URL in config.js"

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // API_URL: 'http://localhost:10000', // For local testing
-    API_URL: 'https://reviveiv-backend.onrender.com', // Update this after deploying to Render
+    API_URL: 'https://revievivbackend.onrender.com', 
 };
 
 window.REVIVE_CONFIG = CONFIG;
